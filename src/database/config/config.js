e229db7e0b91ba43ccbdd0372839d964a8ac9e5d
gabrielsbiche <gabrielsbiche@gmail.com>
2022-09-14 10:18:23 -0300

    feat(database): initialized sequelize-cli and set database settings

---
+++ src/database/config/config.js
@@ -0,0 +1,37 @@
+require("dotenv/config.js");
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: "mysql",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  test: {
+    username: "",
+    password: "",
+    database: "",
+    host: "",
+    port: "",
+    dialect: "",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  production: {
+    username: "",
+    password: "",
+    database: "",
+    host: "",
+    port: "",
+    dialect: "",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+};
